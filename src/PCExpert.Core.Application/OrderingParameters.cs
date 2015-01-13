1e4284865f4d5a8f6c89c6ae50e04b57edc37abe
RegiSV2 <sserregiss@gmail.com>
2015-01-14 00:29:28 +0400

    Forgotten files

---
+++ src/PCExpert.Core.Application/OrderingParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace PCExpert.Core.Application
+{
+	public sealed class OrderingParameters
+	{
+		public OrderingParameters(string orderBy, SortDirection direction)
+		{
+			OrderBy = orderBy;
+			Direction = direction;
+		}
+
+		public string OrderBy { get; private set; }
+
+		public SortDirection Direction { get; private set; }
+	}
+}
---
+++ src/PCExpert.Core.Application/TableParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace PCExpert.Core.Application
+{
+	public class TableParameters
+	{
+		public TableParameters(PagingParameters paging, OrderingParameters ordering)
+		{
+			PagingParameters = paging;
+			OrderingParameters = ordering;
+		}
+
+		public OrderingParameters OrderingParameters { get; private set; }
+
+		public PagingParameters PagingParameters { get; private set; }
+	}
+}
